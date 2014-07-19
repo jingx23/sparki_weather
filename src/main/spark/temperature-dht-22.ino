dbe04a04803de9cd21d539a74f387fadef876d59
jsc <jsc@scireum.de>
2014-07-19 16:37:01 +0200

    Now using dht22 lib for reading (https://github.com/pkourany/idDHT22Spark)

--- src/main/spark/temperature-dht-22.ino
+++ src/main/spark/temperature-dht-22.ino
@@ -1,224 +1,73 @@
-#define MAXTIMINGS 85
-
-#define cli noInterrupts
-#define sei interrupts
-
-#define DHT11 11
-#define DHT22 22
-#define DHT21 21
-#define AM2301 21
-
-#define NAN 999999
-
-class DHT {
-    private:
-        uint8_t data[6];
-        uint8_t _pin, _type, _count;
-        bool read(void);
-        unsigned long _lastreadtime;
-        bool firstreading;
-
-    public:
-        DHT(uint8_t pin, uint8_t type, uint8_t count=6);
-        void begin(void);
-        float readTemperature(bool S=false);
-        float convertCtoF(float);
-        float readHumidity(void);
-
-};
+// This #include statement was automatically added by the Spark IDE.
+#include "idDHT22/idDHT22.h"
 
 
-DHT::DHT(uint8_t pin, uint8_t type, uint8_t count) {
-    _pin = pin;
-    _type = type;
-    _count = count;
-    firstreading = true;
-}
+// declaration for DHT11 handler
+int idDHT22pin = D0; //Digital pin for comunications
+void dht22_wrapper(); // must be declared before the lib initialization
 
+// DHT instantiate
+idDHT22 DHT22(idDHT22pin, dht22_wrapper);
 
-void DHT::begin(void) {
-    // set up the pins!
-    pinMode(_pin, INPUT);
-    digitalWrite(_pin, HIGH);
-    _lastreadtime = 0;
-}
-
-
-//boolean S == Scale.  True == Farenheit; False == Celcius
-float DHT::readTemperature(bool S) {
-    float _f;
-
-    if (read()) {
-        switch (_type) {
-            case DHT11:
-                _f = data[2];
-
-                if(S)
-                    _f = convertCtoF(_f);
-
-                return _f;
-
-
-            case DHT22:
-            case DHT21:
-                _f = data[2] & 0x7F;
-                _f *= 256;
-                _f += data[3];
-                _f /= 10;
-
-                if (data[2] & 0x80)
-                    _f *= -1;
-
-                if(S)
-                    _f = convertCtoF(_f);
-
-                return _f;
-        }
-    }
-
-    return NAN;
-}
-
-
-float DHT::convertCtoF(float c) {
-    return c * 9 / 5 + 32;
-}
-
-
-float DHT::readHumidity(void) {
-    float _f;
-    if (read()) {
-        switch (_type) {
-            case DHT11:
-                _f = data[0];
-                return _f;
-
-
-            case DHT22:
-            case DHT21:
-                _f = data[0];
-                _f *= 256;
-                _f += data[1];
-                _f /= 10;
-                return _f;
-        }
-    }
-
-    return NAN;
-}
-
-
-bool DHT::read(void) {
-    uint8_t laststate = HIGH;
-    uint8_t counter = 0;
-    uint8_t j = 0, i;
-    unsigned long currenttime;
-
-    // pull the pin high and wait 250 milliseconds
-    digitalWrite(_pin, HIGH);
-    delay(250);
-
-    currenttime = millis();
-    if (currenttime < _lastreadtime) {
-        // ie there was a rollover
-        _lastreadtime = 0;
-    }
-
-    if (!firstreading && ((currenttime - _lastreadtime) < 2000)) {
-        //delay(2000 - (currenttime - _lastreadtime));
-        return true; // return last correct measurement
-    }
-
-    firstreading = false;
-    _lastreadtime = millis();
-
-    data[0] = data[1] = data[2] = data[3] = data[4] = 0;
-
-    // now pull it low for ~20 milliseconds
-    pinMode(_pin, OUTPUT);
-    digitalWrite(_pin, LOW);
-    delay(20);
-    cli();
-    digitalWrite(_pin, HIGH);
-    delayMicroseconds(40);
-    pinMode(_pin, INPUT);
-
-    // read in timings
-    for ( i=0; i< MAXTIMINGS; i++) {
-        counter = 0;
-
-        while (digitalRead(_pin) == laststate) {
-            counter++;
-            delayMicroseconds(1);
-
-            if (counter == 255)
-                break;
-        }
-
-        laststate = digitalRead(_pin);
-
-        if (counter == 255)
-            break;
-
-        // ignore first 3 transitions
-        if ((i >= 4) && (i%2 == 0)) {
-            // shove each bit into the storage bytes
-            data[j/8] <<= 1;
-
-            if (counter > _count)
-                data[j/8] |= 1;
-
-            j++;
-        }
-    }
-
-    sei();
-
-
-    // check we read 40 bits and that the checksum matches
-    if ((j >= 40) &&  (data[4] == ((data[0] + data[1] + data[2] + data[3]) & 0xFF)))
-        return true;
-
-
-    return false;
-}
-
-#define DHTPIN D0    // Digital pin D2
-#define DHTTYPE DHT22
-
-DHT dht(DHTPIN, DHTTYPE);
-
-double h;    // humidity
-double t;    // temperature
 char weatherData [700];
 char messageType [5];
-char message [500];
-
-void setup() {
-    //Spark.variable("temp", &t, DOUBLE);
-    //Spark.variable("hum", &h, DOUBLE);
-    //Spark.variable("tempAndHum", &tempAndHum, STRING);
+char message [100];
+void setup()
+{
     Spark.variable("weather", &weatherData, STRING);
-    //Serial.begin(9600);
-    dht.begin();
 }
-
-
-void loop() {
-    h = dht.readHumidity();
-    t = dht.readTemperature();
-
-    if(t==NAN || h==NAN){
-        sprintf(messageType,"ERROR");
-        sprintf(message, "Error reading values.");
-        h = 0;
-        t = 0;
-    }else{
-        sprintf(messageType,"OK");
-        sprintf(message, "");
-    }
-    sprintf(weatherData,"{\"temperature\": %.1f, \"humidity\": %.1f, \"messageType\": %s, \"message\": %s}", t, h, messageType, message);
-    //Serial.print("Temperature: "); Serial.print(t);Serial.print("\n");
-    //Serial.print("Humidity: "); Serial.print(h);Serial.print("\n");
-    //Serial.print("Failure: "); Serial.print(f);Serial.print("\n");
+// This wrapper is in charge of calling
+// mus be defined like this for the lib work
+void dht22_wrapper() {
+	DHT22.isrCallback();
+}
+void loop()
+{
+    sprintf(weatherData,"Hello");
+	//delay(100);
+	DHT22.acquire();
+	while (DHT22.acquiring())
+		;
+	int result = DHT22.getStatus();
+		switch (result)
+	{
+		case IDDHTLIB_OK:
+            sprintf(messageType,"OK");
+		    sprintf(message, "");
+			break;
+		case IDDHTLIB_ERROR_CHECKSUM:
+            sprintf(messageType,"ERROR");
+		    sprintf(message, "Checksum error");
+			break;
+		case IDDHTLIB_ERROR_ISR_TIMEOUT:
+		    sprintf(messageType,"ERROR");
+		    sprintf(message, "ISR Time out error");
+			break;
+		case IDDHTLIB_ERROR_RESPONSE_TIMEOUT:
+            sprintf(messageType,"ERROR");
+		    sprintf(message, "Response time out error");
+			break;
+		case IDDHTLIB_ERROR_DATA_TIMEOUT:
+			sprintf(messageType,"ERROR");
+		    sprintf(message, "Error Data time out error");
+			break;
+		case IDDHTLIB_ERROR_ACQUIRING:
+			sprintf(messageType,"ERROR");
+		    sprintf(message, "Error Acquiring");
+			break;
+		case IDDHTLIB_ERROR_DELTA:
+		    sprintf(messageType,"ERROR");
+		    sprintf(message, "Error Delta time to small");
+			break;
+		case IDDHTLIB_ERROR_NOTSTARTED:
+		    sprintf(messageType,"ERROR");
+		    sprintf(message, "Not started");
+			break;
+		default:
+			sprintf(messageType,"ERROR");
+		    sprintf(message, "Unknown error");
+			break;
+	}
+    sprintf(weatherData,"{\"temperature\": %.1f, \"humidity\": %.1f, \"dewPoint\": %.1f, \"dewPointSlow\": %.1f, \"messageType\": %s, \"message\": %s}", DHT22.getCelsius(), DHT22.getHumidity(), DHT22.getDewPoint(), DHT22.getDewPointSlow(), messageType, message);
+    delay(2000);
 }
